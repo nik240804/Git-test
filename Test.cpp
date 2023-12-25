512ccdb4cb9262829d5e7f0dd5a3b6c1a5c3d774
nik24 <nik240804@mail.ru>
2023-12-25 22:46:19 +0300

    Changes

--- Test.cpp
+++ Test.cpp
@@ -4,33 +4,33 @@ int main() {
     double num1, num2;
     char operation;
 
-    std::cout << "Введите первое число: ";
+    std::cout << "Enter the first number: ";
     std::cin >> num1;
 
-    std::cout << "Введите операцию (+, -, *, /): ";
+    std::cout << "Enter the operation (+, -, *, /): ";
     std::cin >> operation;
 
-    std::cout << "Введите второе число: ";
+    std::cout << "Enter the second number: ";
     std::cin >> num2;
 
     switch(operation) {
         case '+':
-            std::cout << "Результат: " << num1 + num2 << std::endl;
+            std::cout << "Result: " << num1 + num2 << std::endl;
             break;
         case '-':
-            std::cout << "Результат: " << num1 - num2 << std::endl;
+            std::cout << "Result: " << num1 - num2 << std::endl;
             break;
         case '*':
-            std::cout << "Результат: " << num1 * num2 << std::endl;
+            std::cout << "Result: " << num1 * num2 << std::endl;
             break;
         case '/':
             if(num2 != 0)
-                std::cout << "Результат: " << num1 / num2 << std::endl;
+                std::cout << "Result: " << num1 / num2 << std::endl;
             else
-                std::cout << "Ошибка: деление на ноль невозможно." << std::endl;
+                std::cout << "Error: Division by zero is not possible." << std::endl;
             break;
         default:
-            std::cout << "Ошибка: неверная операция." << std::endl;
+            std::cout << "Error: Invalid operation." << std::endl;
             break;
     }
 
